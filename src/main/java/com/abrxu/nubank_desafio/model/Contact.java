84e4bb83fbf6fb1c153c635cd0e56366b840a8ee
abrxu <abrxu@yahoo.com>
2025-04-22 03:25:00 -0300

    add: adicionado entidades (passível à modificação)

---
+++ src/main/java/com/abrxu/nubank_desafio/model/Contact.java
@@ -0,0 +1,61 @@
+package com.abrxu.nubank_desafio.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tbl_contato")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "txt_nickname", length = 35, nullable = false)
+    private String nickname;
+
+    @Column(name = "txt_name", length = 80, nullable = false)
+    private String name;
+
+    @Column(name = "txt_phone_number", length = 20, nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "txt_secondary_phone_number", length = 20)
+    private String secondaryPhoneNumber;
+
+    // Sim, length 320 pois é o máximo que um email pode ter (64 antes do @ e 255 na parte de domínio do email)
+    @Column(name = "txt_email", length = 320, nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "id_customer")
+    @JsonBackReference
+    private Customer customer;
+
+    @Column(name = "dt_registration", nullable = false, updatable = false)
+    private LocalDateTime registrationDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.registrationDate = LocalDateTime.now();
+    }
+
+    @Column(name = "dt_last_updated")
+    private LocalDateTime lastUpdated;
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.lastUpdated = LocalDateTime.now();
+    }
+
+}
---
+++ src/main/java/com/abrxu/nubank_desafio/model/Customer.java
@@ -0,0 +1,58 @@
+package com.abrxu.nubank_desafio.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tbl_customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "txt_nickname", length = 35, nullable = false)
+    private String nickname;
+
+    @Column(name = "txt_name", length = 80, nullable = false)
+    private String name;
+
+    @Column(name = "txt_phone_number", length = 20, nullable = false)
+    private String phoneNumber;
+
+    // Sim, length 320 pois é o máximo que um email pode ter (64 antes do @ e 255 na parte de domínio do email)
+    @Column(name = "txt_email", length = 320, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Contact> contacts;
+
+    @Column(name = "dt_registration", nullable = false, updatable = false)
+    private LocalDateTime registrationDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.registrationDate = LocalDateTime.now();
+    }
+
+    @Column(name = "dt_last_updated")
+    private LocalDateTime lastUpdated;
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.lastUpdated = LocalDateTime.now();
+    }
+
+}
